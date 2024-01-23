aecbece9115af40d6e1e9d0a091acabc258d2782
RushiBhogade <125214989+RushiBhogade@users.noreply.github.com>
2024-01-23 17:36:39 +0530

    commit 2

--- src/Screens/NearbyHospitalsScreen.js
+++ src/Screens/NearbyHospitalsScreen.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { View, ImageBackground, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { View, StyleSheet } from "react-native";
 
-const NearbyHospitals = () => {
+const NearbyHospitals = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("../assets/images/maps.png")} // replace with the actual path to your image
-        style={styles.backgroundImage}
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 18.24531488419371,
+          longitude:75.69770552561076,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+          
+        }}
       >
-        {/* Content inside the ImageBackground goes here */}
-      </ImageBackground>
+        {/* Add markers for nearby hospitals */}
+        <Marker
+          coordinate={{ latitude: 18.24531488419371, longitude: 75.69770552561076 }}
+          title="Hiremath Hospital"
+          description="हिरेमठ हॉस्पिटल"
+        />
+        <Marker
+          coordinate={{ latitude: 18.228583516618773, longitude:  75.68879524723091 }}
+          title="Shraddha Medical Store"
+          description="श्रद्धा मेडिकल स्टोअर"
+        />
+        {/* Add more markers as needed */}
+      </MapView>
     </View>
   );
 };
@@ -18,9 +36,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  backgroundImage: {
+  map: {
     flex: 1,
-    resizeMode: "cover", // or "stretch"
   },
 });
 
